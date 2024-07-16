97d163b834cd2792eca3701612adc41db2523e0b
wande <yewandeadeyemi84@gmail.com>
2024-07-17 00:22:40 +0100

    pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Project from './pages/Project'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
 const App = () => {
   return (
-    <div className='3xl'>App</div>
+<BrowserRouter>
+<Routes>
+  <Route path='/' element={<Home />}/>
+  <Route path='/about' element={<About />}/>
+  <Route path='/sign-in' element={<SignIn />}/>
+  <Route path='/sign-up' element={<SignUp />}/>
+  <Route path='/dashboard' element={<Dashboard />}/>
+  <Route path='/projects' element={<Project />}/>
+  </Routes>
+  </BrowserRouter>
   )
 }
 
