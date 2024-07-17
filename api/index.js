5cf7edbd6f0250a8399f76521251ab14fa627016
wande <yewandeadeyemi84@gmail.com>
2024-07-17 14:24:56 +0100

    connection to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 const  express = require('express')
 const  mongoose = require('mongoose')
+const  dotenv = require('dotenv')
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGO).then(
+    () => {
+        console.log("MongoDb is connected")
+    }).catch((err) => {
+        console.log(err)
+    })
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
