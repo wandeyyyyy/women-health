82674c11b4327cdb2a5aac6be8b82c840c542097
wande <yewandeadeyemi84@gmail.com>
2024-07-26 00:27:52 +0100

    routes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,25 @@
+const User = require('../models/user.model')
+const bcryptjs =  require('bcryptjs')
+
 const signup = async (req, res) => {
  const {username, email, password} = req.body;
- if (!username ||!email || !password || username === "")
+ // check for error
+ if (!username ||!email || !password || username === "",email === "", password === "" ){
+    return res.status(400).json({message: "All field required"})
+ }
+ const hashedPassword = bcryptjs.hashSync(password, 10);
+
+ const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+try{
+  await newUser.save();
+  res.json('Signup successful');
+} catch (error) {
+    res.status(500).json({message: error.message})
+}
 }
 
 module.exports = signup
