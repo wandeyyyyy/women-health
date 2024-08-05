d18d6964b9e741a491620ac03333fe2409876044
wande <yewandeadeyemi84@gmail.com>
2024-08-05 23:55:51 +0100

    sign up ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,13 @@
+
 const User = require('../models/user.model')
 const bcryptjs =  require('bcryptjs')
+const  errorHandler  = require('../utils/error')
 
-const signup = async (req, res) => {
+const signup = async (req, res, next) => {
  const {username, email, password} = req.body;
  // check for error
- if (!username ||!email || !password || username === "",email === "", password === "" ){
-    return res.status(400).json({message: "All field required"})
+ if (!username ||!email || !password || username === "" || email === "" || password === "" ) {
+  next(errorHandler(400, 'All fields are required'));
  }
  const hashedPassword = bcryptjs.hashSync(password, 10);
 
@@ -18,7 +20,7 @@ try{
   await newUser.save();
   res.json('Signup successful');
 } catch (error) {
-    res.status(500).json({message: error.message})
+  next(error)
 }
 }
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react' 
+import {Link} from "react-router-dom"
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='min-h-screen mt-20'>
+      <div className=''>
+        <div className=''>
+        <Link to='/' className='font-bold dark:text-white text-4xl'>
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+            Women's
+            </span>
+            health
+          </Link> 
+        </div>
+        <div className=''>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
