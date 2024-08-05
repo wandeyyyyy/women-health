f82502e10f21f2f698646ebad927b4d732160537
wande <yewandeadeyemi84@gmail.com>
2024-08-06 00:55:47 +0100

    add signup functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,31 @@
 import React from 'react' 
+import { useState } from 'react';
 import {Link} from "react-router-dom"
 import {Label} from "flowbite-react"
 import { TextInput } from 'flowbite-react'
 import { Button } from 'flowbite-react'
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // if (!formData.username || !formData.email || !formData.password) {
+      //   return setErrorMessage('Please fill out all fields.');
+      // }
+    try {
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+    } catch (error) {
+      
+    }
+  }
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -13,7 +34,7 @@ const SignUp = () => {
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
             Women's
             </span>
-            health
+            Health
           </Link> 
           <p className='text-sm font-medium mt-5'>
             This is a demo project. You can sign up with your email and password
@@ -21,14 +42,14 @@ const SignUp = () => {
           </p>
         </div>
         <div className='flex-1'>
-        <form className='flex flex-col gap-4' >
+        <form className='flex flex-col gap-4'onSubmit={handleSubmit} >
         <div>
               <Label value='Your username' />
               <TextInput
                 type='text'
                 placeholder='Username'
                 id='username'
-              
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -38,7 +59,7 @@ const SignUp = () => {
                 type='email'
                 placeholder='name@company.com'
                 id='email'
-           
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -47,7 +68,7 @@ const SignUp = () => {
                 type='password'
                 placeholder='Password'
                 id='password'
-            
+                onChange={handleChange}
               />
             </div>
             <Button
@@ -56,7 +77,7 @@ const SignUp = () => {
               Sign Up
              </Button>
         </form>
-        <div className='flex gap-2 text-sm mt-5'>
+        <div className='flex gap-2 text-sm font-medium mt-5'>
             <span>Have an account?</span>
             <Link to='/sign-in' className='text-blue-500'>
               Sign In
