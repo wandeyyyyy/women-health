50582749279412b69133665b80548bcb306908e9
wande <yewandeadeyemi84@gmail.com>
2024-08-15 13:19:49 +0100

    create sign aoi route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@
 const User = require('../models/user.model')
 const bcryptjs =  require('bcryptjs')
 const  errorHandler  = require('../utils/error')
+const jwt = require('jsonwebtoken')
 
 const signup = async (req, res, next) => {
  const {username, email, password} = req.body;
@@ -24,4 +25,40 @@ try{
 }
 }
 
-module.exports = signup
+
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === '' || password === '') {
+    next(errorHandler(400, 'All fields are required'));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, 'User not found'));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, 'Wrong Credentials'));
+    }
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
+
+    
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = {signup, signin}
