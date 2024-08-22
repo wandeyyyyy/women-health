cb3ed5e7e9121c1813c6db450ecb144343e82c56
wande <yewandeadeyemi84@gmail.com>
2024-08-23 00:12:46 +0100

    make dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Dashboard from './pages/Dashboard'
 import Home from './pages/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import PrivateRoute from './components/PrivateRoute'
 const App = () => {
   return (
 <BrowserRouter>
@@ -17,7 +18,9 @@ const App = () => {
   <Route path='/about' element={<About />}/>
   <Route path='/sign-in' element={<SignIn />}/>
   <Route path='/sign-up' element={<SignUp />}/>
+  <Route element={<PrivateRoute />}>
   <Route path='/dashboard' element={<Dashboard />}/>
+  </Route>
   <Route path='/projects' element={<Project />}/>
   </Routes>
   <Footer/>
